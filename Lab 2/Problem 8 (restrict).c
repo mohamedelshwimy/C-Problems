284e1cdd4faf71a216c9a49a526717630a81ad82
mohamedelshwimy <mohamedahmedebrahim97@gmail.com>
2023-09-26 03:04:22 +0300

    Edit Lab names to be more topic specefic

--- Lab 2/Problem 8 (restrict).c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-void main(void)
-{
-    int num;
-    printf("Enter an integer value (maximum 5 digits): ");
-    scanf("%5d",&num);
-    printf("Entered integer: %d\n", num);
-
-}
